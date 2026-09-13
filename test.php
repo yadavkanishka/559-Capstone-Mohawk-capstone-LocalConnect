6672a72c783b5d0063f9af2c0ad49e66b9a8450c
Kanishka yadav <yadavkanishka0120@gmail.com>
2026-09-13 18:31:02 -0400

    testing Password validation function

--- test.php
+++ test.php
@@ -10,4 +10,15 @@ try {
 } catch (Exception $e) {
     echo "Failed: " . $e->getMessage();
 }
+
+require_once 'includes/validation.php';
+
+echo "<br><br>Password Validation Tests:<br>";
+var_dump(isPasswordValid("password"));      // it should be false
+var_dump(isPasswordValid("Password1"));     // it should be false
+var_dump(isPasswordValid("P@ssword1"));     // it should be true
+
+
 ?>
+
+
