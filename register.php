cdb90c8b1581a95c7b74ec73657a6372f1a57efa
Kanishka yadav <yadavkanishka0120@gmail.com>
2026-09-14 21:54:35 -0400

    error msg and post

--- register.php
+++ register.php
@@ -1,4 +1,9 @@
-
+<form method="POST">
+    Email: <input name="email"><br>
+    Password: <input name="password" type="password"><br>
+    Full Name: <input name="full_name"><br>
+    <button type="submit">Test Submit</button>
+</form>
 
 <?php
 require_once 'config/db.php';
@@ -8,6 +13,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = trim($_POST['email']);
     $password = $_POST['password'];
     $full_name = trim($_POST['full_name']);
+
+    if (!isPasswordValid($password)) {
+        die("Password must be at least 8 characters and include uppercase, lowercase, a number, and a special character.");
+    }
 
     echo "Received: " . $email . " / " . $full_name;
 }
