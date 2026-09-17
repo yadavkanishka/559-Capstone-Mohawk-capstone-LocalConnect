884631f96979cdcb77e10f2990107758c12f3c9d
Kanishka yadav <yadavkanishka0120@gmail.com>
2026-09-17 19:30:05 -0400

    Add application configuration bootstrap
    database initialization files

--- config/config.php
+++ config/config.php
@@ -13,6 +13,12 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
+require_once __DIR__ . '/db.php';
+require_once __DIR__ . '/../includes/functions.php';
+require_once __DIR__ . '/../includes/validation.php';
+require_once __DIR__ . '/../includes/auth.php';
+require_once __DIR__ . '/../includes/db_init.php';
+
 ensure_schema();
 ensure_migrations();
 
