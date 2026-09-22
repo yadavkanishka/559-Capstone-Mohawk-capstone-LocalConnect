9ec1f6d94f3a4446af675446bceddb4ea11283dd
Kanishka yadav <yadavkanishka0120@gmail.com>
2026-09-21 22:05:56 -0400

    Fix registration redirect path
    Updated the registration redirect to use the correct LocalConnect project path after successful registration.

--- register.php
+++ register.php
@@ -1,6 +1,8 @@
 <?php
 require_once __DIR__ . '/config/config.php';
-if (is_logged_in()) redirect('/posts.php');
+$user = ['id' => $conn->lastInsertId()];
+login_user($user);
+redirect('/559-Capstone-Mohawk-capstone-LocalConnect/posts.php');
 
 $message = "";
 
