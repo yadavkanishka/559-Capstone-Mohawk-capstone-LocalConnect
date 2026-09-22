3b9b514426f7d702db7b33c262b9753a21c19d45
Kanishka yadav <yadavkanishka0120@gmail.com>
2026-09-21 20:57:19 -0400

    update auth.php

--- includes/auth.php
+++ includes/auth.php
@@ -1,5 +1,11 @@
 <?php
+<<<<<<< HEAD
+// LocalConnect - 
+// Authentication & role-based access helpers
+
+=======
 // LocalConnect - Authentication & role-based access helpers
+>>>>>>> Registration-W2
 
 function current_user()
 {
@@ -41,6 +47,8 @@ function require_login()
         flash('error', 'Please log in to continue.');
         redirect('/login.php');
     }
+<<<<<<< HEAD
+=======
 }
 
 function require_admin()
@@ -97,5 +105,5 @@ function pending_request_count()
     $stmt->execute([$u['id']]);
 
     return (int)$stmt->fetchColumn();
-}
+>>>>>>> Registration-W2
 }
